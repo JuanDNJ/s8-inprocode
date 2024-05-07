2be6e60904c796543bd2fb86050222e715d80b51
JuanDNJ <juandevnjv@gmail.com>
2024-05-07 20:41:06 +0200

    Añadiendo Google icons svg, back and forwards

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,14 @@
-import { NavLink } from "react-router-dom";
-
+import arrowBack from "../assets/arrow_back.svg";
+import arrowForward from "../assets/arrow_forward.svg";
 export default function Menu() {
   return (
     <nav className="flex items-center justify-center gap-4 p-2">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
+      <button>
+        <img src={arrowBack} alt="Arrow Back" />
+      </button>
+      <button>
+        <img src={arrowForward} alt="Arrow Back" />
+      </button>
     </nav>
   );
 }
